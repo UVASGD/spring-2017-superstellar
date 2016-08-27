0ef0061f30412a73c606aa3407826939062714bd
sanjanamendu <sanjanamendu@gmail.com>
2016-08-27 11:28:58 -0400

    Initial commit

---
+++ Assets/Controls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Controls: MonoBehaviour
+{
+	public GameObject projectile;
+	public Vector2 mProjectileSpeed = new Vector2 (10f, 10f);
+	public Vector2 mSpeed = new Vector2(15, 15);
+	private Vector2 mMovement;
+
+	void Update( )
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+
+			Shoot (1);
+
+		if( Input.GetKeyDown( KeyCode.Alpha2 ) )
+
+			Shoot (2);
+
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+
+			Shoot (3);
+
+		if( Input.GetKeyDown( KeyCode.Alpha4 ) )
+
+			Shoot (4);
+
+		if( Input.GetKeyDown( KeyCode.Alpha5 ) )
+
+			Shoot (5);
+
+//		if( Input.GetKeyUp( KeyCode.Space ) )
+//			Debug.Log( "Space key was released." );
+	}
+
+	void Shoot(int point) {
+//		SpriteRenderer sr = projectile.GetComponent<SpriteRenderer> (); sr.enabled = true;
+		GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		SpriteRenderer sr = proj.GetComponent<SpriteRenderer> (); 
+		sr.enabled = true;
+		Rigidbody rb = proj.GetComponent<Rigidbody> ();
+		rb.AddForce(Vector3.up * 100f);
+
+//		cooldown = Time.time + attackSpeed;
+
+	}
+}
---
+++ Assets/FollowMouseScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouseScript : MonoBehaviour {
+	public Vector3 UltimatePosition = new Vector3(0,0,0);
+	float X, Y;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		X = Input.mousePosition.x;
+		Y = Input.mousePosition.y;
+		Vector3 pos = Input.mousePosition;
+		pos.z = transform.position.z - Camera.main.transform.position.z;
+		transform.position = Camera.main.ScreenToWorldPoint(pos);
+		
+	}
+}
