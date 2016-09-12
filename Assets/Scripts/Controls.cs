8643a72d3bcdd9e9a96a7da670f6c5e9f011c648
sanjanamendu <sanjanamendu@gmail.com>
2016-09-11 22:02:42 -0400

    Revert "commented code and basic hitboxes"
    This reverts commit f71adb1dad67d1a6a31423a5e5c394a54462fbd1.

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -3,10 +3,7 @@
 public class Controls: MonoBehaviour
 {
 	public GameObject projectile;
-	public float velocity;
 
-
-	//Direction Vectors for projectiles
 	private Vector3 top = Vector3.up;
 	private Vector3 righttop = new Vector3(0.95105651629f,0.30901699437f,0);
 	private Vector3 rightbot = new Vector3(0.58778525229f,-0.80901699437f,0);
@@ -14,82 +11,74 @@ public class Controls: MonoBehaviour
 	private Vector3 lefttop = new Vector3(-0.95105651629f,0.30901699437f,0);
 
 
-	//Real-time update. Put conditions you always want to check for here
 	void Update( )
 	{
 		int[] points = getOrientation (transform.rotation);
 
 		if (Input.GetKeyDown (KeyCode.Alpha1))
 
-			Shoot (points[0]); //fires "top" point
+			Shoot (points[0]);
 
 		if( Input.GetKeyDown( KeyCode.Alpha2 ) )
 
-			Shoot (points[1]); //fires "right top" point
+			Shoot (points[1]);
 
 		if (Input.GetKeyDown (KeyCode.Alpha3))
 
-			Shoot (points[2]); //fires "right bottom" point
+			Shoot (points[2]);
 
 		if( Input.GetKeyDown( KeyCode.Alpha4 ) )
 
-			Shoot (points[3]); //fires "left bottom" point
+			Shoot (points[3]);
 
 		if( Input.GetKeyDown( KeyCode.Alpha5 ) )
 
-			Shoot (points[4]); //fires "left top" point
+			Shoot (points[4]);
 	}
 
-
-	//Creates projectile and shoots it in appropriate direction
 	void Shoot(int point) {
 
-		//clones existing projectile gameobject
 		GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
 		SpriteRenderer spr = gameObject.GetComponent<SpriteRenderer> (); 
 		SpriteRenderer sr = proj.GetComponent<SpriteRenderer> (); 
-		Rigidbody2D rb = proj.GetComponent<Rigidbody2D> ();
+		Rigidbody rb = proj.GetComponent<Rigidbody> ();
 
-		//switch statement determines which sprite to use for star and projectile
-		//also adds force to make projectile move
 		switch (point) {
 
 			case 1:
 				spr.sprite = Resources.Load<Sprite>("star_removed");
 				sr.sprite = Resources.Load<Sprite>("star_top");
-				rb.AddForce(top * velocity);
+				rb.AddForce(top * 100f);
 				break;
 			case 2:
 				spr.sprite = Resources.Load<Sprite>("star_removed_right_top");
 				sr.sprite = Resources.Load<Sprite>("star_righttop");
-				rb.AddForce(righttop * velocity);
+				rb.AddForce(righttop * 100f);
 				break;
 			case 3:
 				spr.sprite = Resources.Load<Sprite>("star_removed_right_bottom");
 				sr.sprite = Resources.Load<Sprite>("star_rightbot");
-				rb.AddForce(rightbot * velocity);
+				rb.AddForce(rightbot * 100f);
 				break;
 			case 4:
 				spr.sprite = Resources.Load<Sprite>("star_removed_left_bottom");
 				sr.sprite = Resources.Load<Sprite>("star_leftbot");
-				rb.AddForce(leftbot * velocity);
+				rb.AddForce(leftbot * 100f);
 				break;
 			case 5:
 				spr.sprite = Resources.Load<Sprite>("star_removed_left_top");
 				sr.sprite = Resources.Load<Sprite>("star_lefttop");
-				rb.AddForce(lefttop * velocity);
+				rb.AddForce(lefttop * 100f);
 				break;
 		}
-		sr.enabled = true; //enable sprite render, projectile shows up
+		sr.enabled = true;
 	}
 
-	//Decides orientation of rotating star
 	int[] getOrientation(Quaternion q) {
 
 		int[] points = new int[5];
 		float angle = q.eulerAngles.z;
 
-		//Top point rotates to right 72 degrees. Increments that every if statement
 		if (!(angle < Mathf.Clamp(angle,0.0f,72.0f) || angle > Mathf.Clamp(angle,0.0f,72.0f))) {
 			points = new int[] {1,2,3,4,5};
 		} else if (!(angle < Mathf.Clamp(angle,72.0f,144.0f) || angle > Mathf.Clamp(angle,72.0f,144.0f))) {
@@ -102,7 +91,6 @@ public class Controls: MonoBehaviour
 			points = new int[] {2,3,4,5,1};
 		}
 
-		//return correct order of direction vectors
 		return points;
 
 	}
--- Assets/Scripts/FollowMouseScript.cs
+++ Assets/Scripts/FollowMouseScript.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class FollowMouseScript : MonoBehaviour {
 
-	public float smoothTime; //sets lag for star behind mouse
-	public GameObject bg; //determines bounds for map
+	public float smoothTime;
 	private Vector3 velocity = Vector3.zero;
 	
 	
@@ -18,17 +17,15 @@ public class FollowMouseScript : MonoBehaviour {
 		rotate ();
 	}
 
-	//makes star follow mouse (works fine)
 	void followMouse() {
 		Vector3 pos = Input.mousePosition;
 		pos.z = transform.position.z - Camera.main.transform.position.z;
 		Vector3 target = Camera.main.ScreenToWorldPoint(pos);
-		target.x = Mathf.Clamp (target.x, -bg.transform.localScale.x/2.0f, bg.transform.localScale.x/2.0f);
-		target.y = Mathf.Clamp (target.y, -bg.transform.localScale.y/2.0f, bg.transform.localScale.y/2.0f);
+		target.x = Mathf.Clamp (target.x, -500.0f, 500.0f);
+		target.y = Mathf.Clamp (target.y, -500.0f, 500.0f);
 		transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
 	}
 
-	//rotates top of star to point towards mouse (hypothetically)
 	void rotate() {
 		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
 		Vector3 dir = Input.mousePosition - pos;
