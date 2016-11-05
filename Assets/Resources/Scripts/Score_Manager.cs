d7aeba344e3c677fb21bbaa58dd1af551a6a3bc0
wesdaug23 <wcd5f@virginia.edu>
2016-11-04 20:27:27 -0400

    Did some network building that isn't perfect

--- Assets/Resources/Scripts/Score_Manager.cs
+++ Assets/Resources/Scripts/Score_Manager.cs
@@ -6,14 +6,21 @@ public class Score_Manager : MonoBehaviour {
 
 	public static int score;
 
-	Text text;
+	public GameObject word1;
+
+	public GameObject word2;
+
+	Text text1;
+
+	Text text2;
 
 	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
 
-		text = GetComponent<Text>();
+		text1 = word1.GetComponent<Text>();
+		text2 = word2.GetComponent<Text>();
 
 		score = 0;
 
@@ -24,7 +31,15 @@ public class Score_Manager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		float x = player.transform.position.x;
+		float y = player.transform.position.y - 0.16f*player.transform.localScale.x;
+
+		transform.position = new Vector3 (x, y, 0);
+
+		transform.localScale = player.transform.localScale;
+
 		score = player.GetComponent<Shooting_Controls_edit>().starMass;
-		text.text = "" + score;
+		text1.text = "" + score;
+		text2.text = "" + score;
 	}
 }
