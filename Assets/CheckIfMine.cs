c8a5bd639ace335bcc42896bbb11d48db6d0d10d
sanjanamendu <sanjanamendu@gmail.com>
2016-11-05 01:24:34 -0400

    more networking stuff

---
+++ Assets/CheckIfMine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckIfMine : Photon.MonoBehaviour {
+
+	void OnEnable()
+	{
+		if (this.photonView != null && !this.photonView.isMine) {
+			Debug.Log("entered disable");
+			this.enabled = false;
+			return;
+		}
+	}
+}
