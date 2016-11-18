1479f862ae1fd37396dbb3db3597dd9e4533e999
Divya354 <dsk5df@virginia.edu>
2016-11-18 13:42:50 -0500

    fixed start scene

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void OnStartGame(){
+		Debug.Log ("hello");
+		UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(1);
+}
+}
