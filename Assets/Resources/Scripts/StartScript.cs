6b231e4bb669e73acef81cf1f880b444d92a8948
Divya354 <dsk5df@virginia.edu>
2016-11-19 13:57:47 -0500

    fix menu

--- Assets/Resources/Scripts/StartScript.cs
+++ Assets/Resources/Scripts/StartScript.cs
@@ -4,13 +4,8 @@ using System.Collections;
 public class StartScript : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-
-
-	}
-
-	// Update is called once per frame
-	void Update () {
+	public void OnStartGame () {
 		UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(1);
 	}
+
 }
