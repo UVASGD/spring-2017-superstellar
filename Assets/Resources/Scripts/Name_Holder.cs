4098814aba646b0875bf8819c58c5431c2d7f2b0
Divya354 <dsk5df@virginia.edu>
2017-02-08 20:53:49 -0500

    add Nathaniels name change

---
+++ Assets/Resources/Scripts/Name_Holder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Name_Holder : MonoBehaviour {
+	public void CharacterField(string inputFieldString){
+		PlayerPrefs.SetString ("PlayerName", inputFieldString);
+	}
+}
