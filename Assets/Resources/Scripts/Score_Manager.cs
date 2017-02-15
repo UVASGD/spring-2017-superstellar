8eff4f96746bd7f870a9d6a48160eebd846c2a3c
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2017-02-14 21:55:19 -0500

    Player Name UI

--- Assets/Resources/Scripts/Score_Manager.cs
+++ Assets/Resources/Scripts/Score_Manager.cs
@@ -9,9 +9,11 @@ public class Score_Manager : MonoBehaviour {
 
 	public GameObject word1;
 	public GameObject word2;
+	public GameObject word3;
 
 	Text text1;
 	Text text2;
+	Text text3;
 
 
 	// Use this for initialization
@@ -20,6 +22,8 @@ public class Score_Manager : MonoBehaviour {
 		player = this.gameObject.transform.parent.FindChild("Star").gameObject;
 		text1 = word1.GetComponent<Text>();
 		text2 = word2.GetComponent<Text>();
+		text3 = word3.GetComponent<Text>();
+		text3.text = PlayerPrefs.GetString ("PlayerName");
 		score = 0;
 
 	}
@@ -30,6 +34,7 @@ public class Score_Manager : MonoBehaviour {
 
 		word1.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, 0);
 		word2.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, 0);
+		word3.transform.position = new Vector3 (player.transform.position.x, player.transform.position.y + 0.8f, 0);
 
 		text1.text = "" + score;
 		text2.text = "" + score;
